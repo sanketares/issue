657c808dda9f9bb672edddcfcedef87c198c153d
sanket406 <sanketares@gmail.com>
2024-09-03 03:10:46 -0700

    add ec2

--- main.tf
+++ main.tf
@@ -32,10 +32,7 @@ resource "aws_instance" "sanket_ins" {
   }
 }
 
-resource "aws_instance" "santettesters" {
-  instance_type = "t2.micro"
-  ami           = "ami-053b0d53c279acc90"
-}
+
 
 
 resource "aws_instance" "santettesters" {
