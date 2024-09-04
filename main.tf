84bcd1e2b670c31a945863f11a8ca4597ef8df7b
sanket406 <sanketares@gmail.com>
2024-09-04 01:55:06 -0700

    add

--- main.tf
+++ main.tf
@@ -39,3 +39,10 @@ resource "aws_instance" "santettesters" {
   instance_type = "t2.micro"
   ami           = "ami-053b0d53c279acc90"
 }
+
+
+resource "aws_instan" "santettesters" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+}
+
